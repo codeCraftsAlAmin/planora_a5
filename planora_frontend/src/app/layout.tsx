09dd4c41af90247529188562bfab932652bc3f58
Code Crafts <codecraftsman12@gmail.com>
2026-04-07 01:22:54 +0600

    feat: include top loader

--- planora_frontend/src/app/layout.tsx
+++ planora_frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 import { Footer } from "@/components/shared/footer";
 import { Navbar } from "@/components/shared/navbar";
@@ -19,6 +20,17 @@ export default function RootLayout({
     <html lang="en" className="h-full antialiased">
       <body className="flex min-h-full flex-col">
         <AppProvider>
+          <NextTopLoader
+            color="#0a564a"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px rgba(10, 86, 74, 0.35), 0 0 6px rgba(248, 213, 126, 0.22)"
+            showSpinner={false}
+          />
           <Navbar />
           <main className="flex-1">{children}</main>
           <Footer />
